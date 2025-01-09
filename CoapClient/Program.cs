03472d7b0dbf2561124d07b51b283c6745136007
Matteo Didone <matteo.dido9@gmail.com>
2025-01-09 10:23:23 +0100

    first commit

---
+++ CoapClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using CoAP;
+
+namespace CoapClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crea una richiesta GET per la risorsa "hello"
+            Request request = new Request(Method.GET);
+            request.URI = new Uri("coap://localhost/hello");
+
+            // Invia la richiesta
+            request.Send();
+
+            // Attendi la risposta
+            Response response = request.WaitForResponse();
+
+            if (response != null)
+            {
+                Console.WriteLine("Risposta ricevuta: " + response.ResponseText);
+            }
+            else
+            {
+                Console.WriteLine("Nessuna risposta ricevuta");
+            }
+
+            Console.WriteLine("Premi un tasto per terminare...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CoapServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using CoAP.Server;
+using CoAP.Server.Resources;
+
+namespace MyCoapServer  // Cambiato il nome del namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crea un nuovo server CoAP sulla porta predefinita (5683)
+            var server = new CoapServer();  // Ora non c'è più conflitto
+
+            // Aggiungi la risorsa "hello"
+            server.Add(new HelloResource());
+
+            // Avvia il server
+            server.Start();
+
+            Console.WriteLine("Server CoAP avviato. Premi un tasto per terminare...");
+            Console.ReadKey();
+
+            // Arresta il server quando l'applicazione viene chiusa
+            server.Dispose();
+        }
+    }
+
+    // Definizione della risorsa "hello"
+    class HelloResource : Resource
+    {
+        public HelloResource() : base("hello")
+        {
+            Attributes.Title = "Hello Resource";
+        }
+
+        protected override void DoGet(CoapExchange exchange)
+        {
+            exchange.Respond("Hello, CoAP!");
+        }
+    }
+}
