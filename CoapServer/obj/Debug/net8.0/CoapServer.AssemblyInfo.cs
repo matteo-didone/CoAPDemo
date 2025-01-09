335ad72abfaf9e4d77052336fab4081fe8b209bd
Matteo Didone <matteo.dido9@gmail.com>
2025-01-09 15:16:58 +0100

    Latest updates

--- CoapServer/obj/Debug/net8.0/CoapServer.AssemblyInfo.cs
+++ CoapServer/obj/Debug/net8.0/CoapServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoapServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+972f28335984261f5689b4c4118db5877d0ac94b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1930c30b5dbb7168eb7239629b51bbe9db1e53e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoapServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoapServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
